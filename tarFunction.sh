ae2f923d2bc455d834ca603d8f3a2412389ebcda
mikej-edu <93589607+mikej-edu@users.noreply.github.com>
2021-11-04 22:58:48 +0100

    modified checksum

--- tarFunction.sh
+++ tarFunction.sh
@@ -1,26 +1,22 @@
 #!/bin/bash
+#!/bin/bash
 
 ## Function to archive and compress directories with tar and gzip.
 ## Add your directories to the variable source or change the variable to suite your script.
 
 tarFunction() {
 
-source=    					    # Example $HOME/mystuff/
-destination=   					# Example $HOME
-filename=     					# Example "archive.tar.gz"
-archive=$destination$filename
+source=	      					        # Example $HOME/mystuff/
+archive=./         					# Example "archive.tar.gz"
 
 
-## Check if the source and destination directories exists otherwise exit.
+## Check if the source directory exist otherwise exit.
 
 if [[ -z $source ]]; then
     echo "Source directory is empty" && exit 1
-elif [[ -z $destination ]]; then 
-    echo "Destination directory is empty" && exit 1
+
 elif [[ ! -d $source ]]; then
     echo "$source doesn't exist" && exit 1
-elif [[ ! -d $destination ]]; then
-    echo "$destination doesn't exist" && exit 1
 fi
 
 # Create an archive if the archive file doesn't already exist.
@@ -32,7 +28,7 @@ else
 	tar -czf $archive $source >/dev/null 2>&1 && (command sha512sum $archive > $archive.CHECKSUM)
 
 fi
-echo "hey"
+
 ## Check if CHECKSUM is correct
 
 if [[ -f $archive.CHECKSUM ]]; then
@@ -43,4 +39,3 @@ else
 fi
 }
 
-tarFunction
