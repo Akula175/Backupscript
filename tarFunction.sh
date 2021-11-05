fbb5b351b6d0cf84f3ac915b62425fdc45ab005f
mikej-edu <93589607+mikej-edu@users.noreply.github.com>
2021-11-05 11:31:24 +0100

    Minor changes

--- tarFunction.sh
+++ tarFunction.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 ## Function to archive and compress directories with tar and gzip.
-## Add your directories to the variable source or change the variable to suite your script.
+## Add your directories to the variable source or change the variable to suit your script.
 
 tarFunction() {
 
-source=	      					        # Example $HOME/mystuff/
-archive=./         					# Example "archive.tar.gz"
+source=    					    # Example $HOME/mystuff/
+archive=./     					# Example "archive.tar.gz"
 
 
 ## Check if the source directory exist otherwise exit.
@@ -14,7 +14,7 @@ archive=./         					# Example "archive.tar.gz"
 if [[ -z $source ]]; then
     echo "Source directory is empty" && exit 1
 
-elif [[ ! -d $source ]]; then
+else [[ ! -d $source ]]; 
     echo "$source doesn't exist" && exit 1
 fi
 
@@ -24,14 +24,14 @@ if [[ -f $archive ]]; then
 	echo "The file $archive already exists...exiting" && exit 1
 
 else
-	tar -czf $archive $source >/dev/null 2>&1 && (command sha512sum $archive > $archive.CHECKSUM)
-
+	tar -cpzf $archive -C $source . >/dev/null 2>&1 && (command sha512sum $archive > $archive.CHECKSUM)
 fi
 
 ## Check if CHECKSUM is correct
 
 if [[ -f $archive.CHECKSUM ]]; then
-	command sha512sum -c $archive.CHECKSUM
+    command sha512sum -c $archive.CHECKSUM >/dev/null 2>&1 && echo success || echo failed
+
 
 else
 	echo "The archive file and checksum file doesn't match" && exit 1
