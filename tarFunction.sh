620a0d5332d1ea3ae1352f68cecd476d7f2016d5
mikej-edu <93589607+mikej-edu@users.noreply.github.com>
2021-11-05 12:35:56 +0100

    Cleaned up, added hostname

--- tarFunction.sh
+++ tarFunction.sh
@@ -5,35 +5,28 @@
 
 tarFunction() {
 
-source=                         					    # Example $HOME/mystuff/
-archive=./'backup_'$(date +"%Y-%m-%d_%H%M")'.tar.gz'
+source=                        					    # Example $HOME/mystuff/
+archive=./$HOSTNAME'_'$(date +"%Y-%m-%d_%H%M%S")'.tar.gz'
 
 
 ## Check if the source directory exist otherwise exit.
+## if the source directory exist, create the archive.
 
 if [[ -z $source ]]; then
     echo "Source directory is empty" && exit 1
-
 elif [[ ! -d $source ]]; then
     echo "$source doesn't exist" && exit 1
-fi
-
-# Create an archive if the archive file doesn't already exist.
-
-if [[ -f $archive ]]; then
-	echo "The file $archive already exists...exiting" && exit 1
-
 else
 	tar -cpzf $archive -C $source . >/dev/null 2>&1 && (command sha512sum $archive > $archive.CHECKSUM)
 fi
+
 
 ## Check if CHECKSUM is correct
 
 if [[ -f $archive.CHECKSUM ]]; then
     command sha512sum -c $archive.CHECKSUM >/dev/null 2>&1 && echo success || echo failed
-
-
 else
 	echo "The archive file and checksum file doesn't match" && exit 1
 fi
+
 }
