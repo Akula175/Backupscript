8eaaea3ee5be4718859c8f3f4da3b6bd2429bbff
Tobias <tobias.gus99@gmail.com>
2021-11-06 21:03:58 +0100

    Added function for scp zipping

--- script.sh
+++ script.sh
@@ -12,6 +12,68 @@ LDIR=$HOME/backup
 KEY=~/.ssh/id_rsa.pub
 
 
+
+## Function to archive and compress directories with tar and gzip.
+## Add your directories to the variable source or change the variable to suit your script.
+
+tarFunction() {
+
+    source=$1                      					   
+    archive="$LDIR"/$HOSTNAME'_'$(date +"%Y-%m-%d_%H%M%S")'.tar.gz'
+
+
+    ## Check if the source directory exist otherwise exit.
+    ## if the source directory exist, create the archive.
+
+    if [[ -z $source ]]; then
+        echo "Source directory is empty" && exit 1
+    elif [[ ! -d $source ]]; then
+        echo "$source doesn't exist" && exit 1
+    else
+        tar -cpzf $archive -C $source . >/dev/null 2>&1 && (command sha512sum $archive > $archive.CHECKSUM)
+    fi
+
+
+    ## Check if CHECKSUM is correct
+
+    if [[ -f $archive.CHECKSUM ]]; then
+        command sha512sum -c $archive.CHECKSUM >/dev/null 2>&1 && echo success || echo failed
+    else
+        echo "The archive file and checksum file doesn't match" && exit 1
+    fi
+
+}
+
+
+tarscpFunction() {
+
+    source=$LDIR/$2                     					   
+    archive="$LDIR"/$HOSTNAME'_'$(date +"%Y-%m-%d_%H%M%S")'.tar.gz'
+
+
+    ## Check if the source directory exist otherwise exit.
+    ## if the source directory exist, create the archive.
+
+    if [[ -z $source ]]; then
+        echo "Source directory is empty" && exit 1
+    elif [[ ! -d $source ]]; then
+        echo "$source doesn't exist" && exit 1
+    else
+        tar -cpzf $archive -C $source . >/dev/null 2>&1 && (command sha512sum $archive > $archive.CHECKSUM)
+    fi
+
+
+    ## Check if CHECKSUM is correct
+
+    if [[ -f $archive.CHECKSUM ]]; then
+        command sha512sum -c $archive.CHECKSUM >/dev/null 2>&1 && echo success || echo failed
+    else
+        echo "The archive file and checksum file doesn't match" && exit 1
+    fi
+
+}
+
+
 # Checks if backup Directory exists, otherwise it gets created (silent)
 
 if [ ! -d $LDIR ]; then
@@ -22,24 +84,15 @@ fi
 # If valid Dir, begins tar
 
 if [[ $1 =~ [/][a-z] ]] && [[ ! $1 =~ [0-9] ]]; then
-  if [ -d "$1" ]; then
-    echo "Input is valid Directory, creating backup..."
-    #tar ....
-  else
-    echo "$1 is not a working Directory" && exit 1
+    tarFunction "$1"
 fi
 
 # Checks if input is an IP addr
 # If valid IP, begins scp or Rsync
 
-else
-  if [[ $1 =~ [a-z]@[0-9] ]]; then
+if [[ $1 =~ [a-z]@[0-9] ]]; then
     echo "Entered IP address, starting scp"
     scp -r -i $KEY $1:$2 $LDIR
-    
-    else 
-    echo "Please input IP addr or working DIR" && exit 1
-  
-  fi 
-
+    tarscpFunction "$2"
+    echo $LDIR$2
 fi
