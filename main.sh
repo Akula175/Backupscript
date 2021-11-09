90ec294af2037f6840a910c83b9c214830c30fec
Tobias <tobias.gus99@gmail.com>
2021-11-09 22:07:53 +0100

    Fixed error

--- main.sh
+++ main.sh
@@ -47,7 +47,7 @@ tarFunction() {
     elif [[ ! -d $SDIR ]]; then
         echo "$SDIR doesn't exist" && exit 1
     else
-        tar -cvzfp $ARCHIVE -C $SDIR . >/dev/null 2>&1 && (command sha512sum $ARCHIVE > $ARCHIVE.CHECKSUM)
+        tar -cpzf $ARCHIVE -C $SDIR . >/dev/null 2>&1 && (command sha512sum $ARCHIVE > $ARCHIVE.CHECKSUM)
     fi
 
 
@@ -76,7 +76,7 @@ tarscpFunction() {
     elif [[ ! -d $SDIR ]]; then
         echo "$SDIR doesn't exist" && exit 1
     else
-        tar -cvzfp $ARCHIVE -C $SDIR . >/dev/null 2>&1 && (command sha512sum $archive > $archive.CHECKSUM)
+        tar -cpzf $ARCHIVE -C $SDIR . >/dev/null 2>&1 && (command sha512sum $archive > $archive.CHECKSUM)
     fi
 
 
