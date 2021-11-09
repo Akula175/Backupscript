e8ec75ed72bf396652295efd28053e635e10f727
mikej-edu <93589607+mikej-edu@users.noreply.github.com>
2021-11-09 17:17:37 +0100

    Created a beta main
    Moved some content to functions for beta testing.

---
+++ betamain.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+
+# Reads Input from user using flags in this order:
+# "/local/directory" OR "user@server /remote/directory"
+
+# Variable for local backup folder. Change this if you want the backup to save in a different location
+
+source ./functions.sh
+
+source=$1
+
+LDIR=$HOME/backup
+
+# Variable for Key. Change this if your ssh key is in a different location
+
+KEY=~/.ssh/mypubkey.pub
+
+# Variable for TEMP location
+
+TEMP=/tmp/temp
+
+# Check if Cron and rsync are installed otherwise exit
+
+function cronChk
+function rsyncChk
+
+# Checks if backup Directory exists, otherwise it gets created (silent)
+# Also checks if temp Directory exists, otherwise this will also get created
+
+if [ ! -d $LDIR ]; then
+    mkdir $LDIR
+if [ ! -d $TEMP ]; then
+    mkdir $TEMP 
+fi
+
+fi
+
+## Activates encryption if "-e" flag is set.
+function encryptFunction
+
+## Activates decryption if "-d" flag is set.
+function decryptFunction
+
+# Checks if input is a working Directory
+# If valid Dir, begins tar
+
+if [[ $source =~ [/][a-z] ]] && [[ ! $source =~ [0-9] ]]; then
+    tarFunction 
+fi
+
+# Checks if input is an IP addr
+# If valid IP, begins scp or Rsync
+
+if [[ $source =~ [a-z]@[0-9] ]]; then
+    echo "Entered IP address, starting scp"
+    rsync -zarvh -e "ssh -i $KEY" $source:$2 $TEMP
+    tarscpFunction 
+    rm -rf $TEMP/*
+fi
