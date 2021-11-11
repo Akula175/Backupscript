6557b4a1f30f6a1b62bf97008d5e967074008f71
mikej-edu <93589607+mikej-edu@users.noreply.github.com>
2021-11-11 14:00:06 +0100

    Merged with updated variable names and fixes

--- main.sh
+++ main.sh
@@ -3,17 +3,49 @@
 # Reads Input from user using flags in this order:
 # "/local/directory" OR "user@server /remote/directory"
 
-# Variable for local backup folder. Change this if you want the backup to save in a different location
+source ./functions.sh
 
-LDIR=$HOME/backup
+LDIR=$HOME/backup            # Variable for local backup folder. Change this if you want the backup to save in a different location
+KEY=~/.ssh/mypubkey.pub      # Variable for Key. Change this if your ssh key is in a different location
+TEMP=/tmp/temp               # Variable for TEMP location
 
-# Variable for Key. Change this if your ssh key is in a different location
 
-KEY=~/.ssh/id_rsa.pub
+## Checks if -e flag or -d flag are set from user input.
+while getopts "ed" FLAGS
+do
+    case $FLAGS in
+        e) 
+            FLAG_E=$1 ## Activates encryption
+            shift
+            ;;
+        d) 
+            FLAG_D=$1 ## Activates decryption
+            shift
+            ;;
+        r)
+            FLAG_R=$1 ## Activates restore function
+            shift
+            ;;
+    esac
+done
 
-# Variable for TEMP location
 
-TEMP=/tmp/temp
+SDIR=$1                    # Variable for files to backup, reads from user input.
+
+
+# Check if Cron and rsync are installed otherwise exit
+cronCheck=$(crontab -V 2>/dev/null)
+rsyncCheck=$(rsync -V 2>/dev/null)
+
+if [[ ! $cronCheck ]]; then
+    echo "cron is not installed"    
+    exit 1
+fi
+
+if [[ ! $rsyncCheck ]]; then
+    echo "rsync is not installed"
+    exit 1
+fi
 
 
 # Checks if backup Directory exists, otherwise it gets created (silent)
@@ -21,96 +53,48 @@ TEMP=/tmp/temp
 
 if [ ! -d $LDIR ]; then
     mkdir $LDIR
+fi
+
 if [ ! -d $TEMP ]; then
     mkdir $TEMP 
-fi
-
-fi
-
-
-
-# Function to archive and compress directories with tar and gzip
-# Source == Directory that should be zipped
-# Archive == Destination Directory
-
-tarFunction() {
-
-    source=$1                      					   
-    archive="$LDIR"/$HOSTNAME'_'$(date +"%Y-%m-%d_%H%M%S")'.tar.gz'
-
-
-    ## Check if the source directory exist otherwise exit.
-    ## if the source directory exist, create the archive.
-
-    if [[ -z $source ]]; then
-        echo "Source directory is empty" && exit 1
-    elif [[ ! -d $source ]]; then
-        echo "$source doesn't exist" && exit 1
-    else
-        tar -cpzf $archive -C $source . >/dev/null 2>&1 && (command sha512sum $archive > $archive.CHECKSUM)
-    fi
-
-
-    ## Check if CHECKSUM is correct
-
-    if [[ -f $archive.CHECKSUM ]]; then
-        command sha512sum -c $archive.CHECKSUM >/dev/null 2>&1 && echo success || echo failed
-    else
-        echo "The archive file and checksum file doesn't match" && exit 1
-    fi
-
-}
-
-
-tarscpFunction() {
-
-    source=$TEMP                     					   
-    archive="$LDIR"/$HOSTNAME'_'$(date +"%Y-%m-%d_%H%M%S")'.tar.gz'
-
-
-    # Check if the source directory exist otherwise exit
-    # if the source directory exist, create the archive
-
-    if [[ -z $source ]]; then
-        echo "Source directory is empty" && exit 1
-    elif [[ ! -d $source ]]; then
-        echo "$source doesn't exist" && exit 1
-    else
-        tar -cpzf $archive -C $source . >/dev/null 2>&1 && (command sha512sum $archive > $archive.CHECKSUM)
-    fi
-
-
-    # Check if CHECKSUM is correct
-
-    if [[ -f $archive.CHECKSUM ]]; then
-        command sha512sum -c $archive.CHECKSUM >/dev/null 2>&1 && echo success || echo failed
-    else
-        echo "The archive file and checksum file doesn't match" && exit 1
-    fi
-
-}
-
-
-
-#Checks if the "-e" flag is used
-
-if [[ $1 == "-e" ]]; then 
-    echo "Success"
 fi
 
 # Checks if input is a working Directory
 # If valid Dir, begins tar
 
-if [[ $1 =~ [/][a-z] ]] && [[ ! $1 =~ [0-9] ]]; then
-    tarFunction "$1"
+if [[ $SDIR =~ [/][a-z] ]] && [[ ! $SDIR =~ [0-9] ]]; then
+   tarFunction
 fi
+
+
+## Activates encryption if "-e" flag is set.
+encryptFunction
+
+
+## Activates decryption if "-d" flag is set.
+decryptFunction
 
 # Checks if input is an IP addr
 # If valid IP, begins scp or Rsync
 
-if [[ $1 =~ [a-z]@[0-9] ]]; then
+if [[ $SDIR =~ [a-z]@[0-9] ]]; then
     echo "Entered IP address, starting scp"
-    rsync -zarvh -e "ssh -i $KEY" $1:$2 $TEMP
+    rsync -zarvh -e "ssh -i $KEY" $SDIR:$2 $TEMP
     tarscpFunction 
     rm -rf $TEMP/*
+fi
+
+# Restore prompt
+
+if [[ $FLAG_R ]]; then 
+    cd $LDIR
+    ls *.gz
+    read -p "Which file do you want to restore?: " LINE2
+    
+    if [[ ! -e "$LINE2" ]]; then 
+        echo "Input is not a valid file." && exit 1
+    else
+        restoreFunction
+        rm -rf $TEMP/*
+    fi
 fi
