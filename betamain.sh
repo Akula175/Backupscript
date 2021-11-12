2228343e065c56c1aef2635dbe4c65088b5a1d54
mikej-edu <93589607+mikej-edu@users.noreply.github.com>
2021-11-12 12:51:11 +0100

    Added helpFunction and changed how flags work

--- betamain.sh
+++ betamain.sh
@@ -9,28 +9,46 @@ LDIR=$HOME/backup            # Variable for local backup folder. Change this if 
 KEY=~/.ssh/mypubkey.pub      # Variable for Key. Change this if your ssh key is in a different location
 TEMP=/tmp/temp               # Variable for TEMP location
 
+if [[ ${#} -eq 0 ]]; then
+helpFunction
+exit 0
+fi
 
-## Checks if -e flag or -d flag are set from user input.
-while getopts "edr" FLAGS
+while [[ ! $# -eq 0 ]]
 do
-    case $FLAGS in
-        e) 
+    case "$1" in
+        --help | -h)
+            helpFunction
+            exit 0
+            ;;
+        --encrypt | -e)
+            if [[ "$2" ]]; then
+                SDIR=$2
+            fi
             FLAG_E=$1 ## Activates encryption
-            shift
             ;;
-        d) 
-            FLAG_D=$1 ## Activates decryption
-            shift
+        --decrypt | -d)
+             FLAG_D=$1 ## Activates decryption
+            #shift
             ;;
-        r)
+        --restore | -r)
+            if [[ $2 ]]; then
+                SDIR=$2
+            fi
             FLAG_R=$1 ## Activates restore function
-            shift
+            #shift
             ;;
+#        --ssh | -s)
+#            if [[ "$2" ]]; then
+#            fi  
+#            ;;
     esac
+    shift
 done
 
-
-SDIR=$1                    # Variable for files to backup, reads from user input.
+#if [[ ! $1 -eq "" ]]; then
+#    SDIR=$1                    # Variable for files to backup, reads from user input.
+#fi
 
 
 # Check if Cron and rsync are installed otherwise exit
@@ -83,7 +101,7 @@ decryptFunction
 if [[ $2 =~ [a-z]@[0-9] ]]; then
     echo "Entered IP address, starting scp"
     rsync -zarvh -e "ssh -i $KEY" $2:$SDIR $TEMP
-    ARCHSRC=$TEMP
+    ARCHSRCH=$TEMP
     tarFunction 
     rm -rf $TEMP/*
 fi
